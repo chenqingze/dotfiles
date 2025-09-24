4008cee9b5ecd129d648367f326fc62f57c3a82a
ChenQingze <chenqingze107@gmail.com>
2025-09-24 10:09:49 +0800

    remove p10k

--- home/dot_zsh/zsh.sh
+++ home/dot_zsh/zsh.sh
@@ -1,7 +1,7 @@
 # manually load configs 
 export CONFIG_DIR="$HOME/.zsh/configs"
 source "$CONFIG_DIR/base.zsh"
-source "$CONFIG_DIR/theme.zsh"
+#source "$CONFIG_DIR/theme.zsh"
 source "$CONFIG_DIR/plugins.zsh"
 source "$CONFIG_DIR/aliases.zsh"
 
